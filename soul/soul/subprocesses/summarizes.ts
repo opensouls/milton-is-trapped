8494da6579a24071eb2903535662a22d835730fe
thiago <371426+dooart@users.noreply.github.com>
2024-04-04 19:47:40 -0300

    upgrade to new api

--- soul/soul/subprocesses/summarizes.ts
+++ soul/soul/subprocesses/summarizes.ts
@@ -3,75 +3,90 @@ import {
   MentalProcess,
   WorkingMemory,
   createCognitiveStep,
+  indentNicely,
   useActions,
   useProcessMemory,
 } from "@opensouls/engine";
 import internalMonologue from "../lib/internalMonologue.js";
-import { prompt } from "../lib/prompt.js";
 
 const summaryOfSeriesOfEvents = createCognitiveStep((existing: string) => {
   return {
     command: ({ soulName }: WorkingMemory) => {
-      return prompt`
-      ## Existing notes
-      ${existing}
+      return {
+        role: ChatMessageRoleEnum.System,
+        content: indentNicely`
+          ## Existing notes
+          ${existing}
 
-      ## Description
-      Write an updated and clear paragraph describing everything that happened so far.
-      Make sure to keep details that ${soulName} would want to remember.
+          ## Description
+          Write an updated and clear paragraph describing everything that happened so far.
+          Make sure to keep details that ${soulName} would want to remember.
 
-      ## Rules
-      * Keep descriptions as a paragraph
-      * Keep relevant information from before
-      * Use abbreviated language to keep the notes short
-      * Make sure to detail the motivation of ${soulName} (what are they trying to accomplish, what have they done so far).
+          ## Rules
+          * Keep descriptions as a paragraph
+          * Keep relevant information from before
+          * Use abbreviated language to keep the notes short
+          * Make sure to detail the motivation of ${soulName} (what are they trying to accomplish, what have they done so far).
 
-      Please reply with the updated notes on the series of events:
-  `;
+          Please reply with the updated notes on the series of events:
+      `,
+      };
     },
   };
 });
 
 const summarizesSeriesOfEvents: MentalProcess = async ({ workingMemory }) => {
-  let memory = workingMemory;
-
-  const seriesOfEventsModel = useProcessMemory(prompt`
-    ${memory.soulName} is experiencing a series of events and is trying to learn as much as possible about them.
+  const conversationModel = useProcessMemory(indentNicely`
+    ${workingMemory.soulName} is experiencing a series of events and is trying to learn as much as possible about them.
   `);
   const { log: engineLog } = useActions();
   const log = (...args: any[]) => {
     engineLog("[summarizes]", ...args);
   };
 
-  let finalMemory = memory;
-
-  if (memory.memories.length > 10) {
-    log("Updating notes");
-    [memory] = await internalMonologue(memory, { instructions: "What have I learned so far.", verb: "noted" });
-
-    const updatedNotes = await step.compute(summaryOfSeriesOfEvents(seriesOfEventsModel.current));
-    seriesOfEventsModel.current = updatedNotes as string;
-
-    return finalMemory.withUpdatedMemory(async (memories) => {
-      const newMemories = memories.flat();
-      return [
-        newMemories[0],
-        {
-          role: ChatMessageRoleEnum.Assistant,
-          content: prompt`
-            ## Events so far
-            ${updatedNotes}
-          `,
-          metadata: {
-            conversationSummary: true,
-          },
-        },
-        ...newMemories.slice(-8),
-      ];
+  if (workingMemory.memories.length > 15) {
+    log("summarizing series of events");
+    const [withMemoryThoughts] = await internalMonologue(workingMemory, {
+      instructions: `What have I learned so far.`,
+      verb: "noted",
     });
+
+    const [, updatedNotes] = await summaryOfSeriesOfEvents(withMemoryThoughts, conversationModel.current);
+    log("done summarizing");
+
+    conversationModel.current = updatedNotes as string;
+
+    if (workingMemory.find((m) => !!m.metadata?.conversationSummary)) {
+      return workingMemory
+        .map((m) => {
+          if (m.metadata?.conversationSummary) {
+            return {
+              ...m,
+              content: updatedNotes,
+            };
+          }
+          return m;
+        })
+        .slice(0, 2)
+        .concat(workingMemory.slice(-8));
+    }
+
+    return workingMemory
+      .slice(0, 1)
+      .withMemory({
+        role: ChatMessageRoleEnum.Assistant,
+        content: indentNicely`
+          ## Conversation so far
+          ${updatedNotes}
+        `,
+        metadata: {
+          conversationSummary: true,
+        },
+      })
+      .concat(workingMemory.slice(-8));
   }
 
-  return finalMemory;
+  return workingMemory;
 };
 
 export default summarizesSeriesOfEvents;
